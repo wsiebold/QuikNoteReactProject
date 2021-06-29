f3707cc409e6779ea7debbc7fb49217ba8778490
Bill Siebold <email@designpdx.com>
2021-06-29 11:29:20 -0700

    modifying files to simplify boilerplate

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,26 +4,9 @@ function Heading() {
   const date = new Date();
   const currentTime = date.getHours();
 
-  let greeting;
-
-  const customStyle = {
-    color: ""
-  };
-
-  if (currentTime < 12) {
-    greeting = "It is the morning";
-    customStyle.color = "red";
-  } else if (currentTime < 18) {
-    greeting = "It is the afternoon";
-    customStyle.color = "green";
-  } else {
-    greeting = "It is the evening";
-    customStyle.color = "blue";
-  }
-
-  return (
-    <h1 className="heading" style={customStyle}>
-      {greeting}
+return (
+    <h1 className="heading">
+      Quik Note (Keeper) App
     </h1>
   );
 }
